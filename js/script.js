161baabdf857b687ba02d166f91136e151bef468
LLlTOPM <llltopm666@gmail.com>
2018-07-27 01:33:21 +0500

    commit #2 Display cells and click on them

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-//test commit2
+var boardCell = document.getElementsByClassName('boardCell');
+var displayedSymbol = 'X';
+
+for (var i=0; i < boardCell.length; i++) {
+    boardCell[i].addEventListener('click', welcome);
+}
+
+function welcome() {
+    console.log("clicked on " + this.id)
+    this.innerHTML = displayedSymbol;
+    displayedSymbol = switcher(displayedSymbol);
+}
+
+function switcher(displayedSymbol) {
+    if (displayedSymbol == 'X') {
+        return 'O'
+    } else {
+        return 'X'
+    }
+}
