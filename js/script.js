c0036e9fe8b517056c7ed82597c51cc14c3fe649
LLlTOPM <llltopm666@gmail.com>
2018-07-28 20:57:53 +0500

    commit #2 Display cells and click on them

--- js/script.js
+++ js/script.js
@@ -1,20 +1,95 @@
-var boardCell = document.getElementsByClassName('boardCell');
-var displayedSymbol = 'X';
+var displayedSymbol = 'X',
+    startGameButton = document.getElementById('startGame'),
+    minutes = 0,
+    seconds = 0,
+    timer;
 
-for (var i=0; i < boardCell.length; i++) {
-    boardCell[i].addEventListener('click', welcome);
-}
+startGameButton.onclick = function () {
+    console.log("Game started");
 
-function welcome() {
-    console.log("clicked on " + this.id)
-    this.innerHTML = displayedSymbol;
-    displayedSymbol = switcher(displayedSymbol);
-}
+    var div = document.createElement('div');
+    div.className = "board";
+    document.body.replaceChild(div, startGameButton)
 
-function switcher(displayedSymbol) {
-    if (displayedSymbol == 'X') {
-        return 'O'
-    } else {
-        return 'X'
+    for (var i=0; i<9 ; i++) {
+        div = document.createElement('div');
+        div.className = "boardCell";
+        div.id = "'cell_"+i;
+        div.addEventListener('click', marked)
+        document.getElementsByClassName("board")[0].appendChild(div);
     }
+
+    var input = document.createElement('input');
+    input.type = "button";
+    input.value = "Новая игра";
+    input.onclick = reset;
+    document.body.appendChild(input);
+
+    div = document.createElement('div');
+    div.id = "timer";
+    div.innerText = "00:00";
+    document.body.appendChild(div);
+
+    timer = setInterval(timerStep, 1000);
+}
+
+function timerStep() {
+    var timer = document.getElementById('timer'),
+        result,
+        visibleMinutes,
+        visibleSeconds;
+    seconds += 1;
+    if (seconds > 59) {
+        minutes += 1;
+        seconds = 0;
+    }
+    if (minutes > 59) {
+        reset();
+    }
+
+    if (seconds < 10){
+        visibleSeconds = "0" + seconds;
+    } else {
+        visibleSeconds = seconds;
+    }
+
+    if (minutes < 10) {
+        visibleMinutes = "0"+minutes;
+    } else {
+        visibleMinutes = minutes;
+    }
+
+    result = visibleMinutes + ":" + visibleSeconds;
+    timer.innerText = result;
+}
+
+function reset() {
+    console.log("reseted");
+    var boardCell = document.getElementsByClassName('boardCell'),
+        timer = document.getElementById('timer');
+    for (var i=0; i<boardCell.length; i++){
+        boardCell[i].innerText = "";
+    }
+    timer.innerText = "00:00";
+    minutes = 0;
+    seconds = 0;
+}
+
+function marked() {
+    if (this.innerText === ""){
+        this.innerText = displayedSymbol;
+        displayedSymbol = switcherDisplayedSymbol(displayedSymbol);
+    } else {
+        console.log(this.id + " is not empty");
+    }
+    checkEndOfGame ();
+}
+
+function switcherDisplayedSymbol(displayedSymbol) {
+    if (displayedSymbol == 'X') return 'O'
+        return 'X'
+}
+
+function checkEndOfGame() {
+
 }
